47cd91b811dedd8262f2865950f426fc2efb37e6
Aaronmaster125 <139798365+Aaronmaster125@users.noreply.github.com>
2023-07-17 14:30:45 -0400

    Added basic horizontal movement to player

--- Aerial Ascent/Assets/Scripts/PlayerController.cs
+++ Aerial Ascent/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    public float speed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
+
+    private void Walk(Vector2 dir)
+    {
+        rb.velocity = (new Vector2(dir.x * speed, rb.velocity.y));
+
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector2 dir = new Vector2(x, y);
+
+        Walk(dir);
     }
 }
