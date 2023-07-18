9fd2ad31edc6bb771ca9b355ff16d9c8bacdebb7
Fifi Art <fifiinart@gmail.com>
2023-07-17 20:23:02 -0400

    Animate player between idle and run animations

--- Aerial Ascent/Assets/Scripts/PlayerController.cs
+++ Aerial Ascent/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Components
     private Rigidbody2D rb;
+    private Animator anim;
+
+    [Header("Movement")]
     public float speed = 10f;
+
+    [Header("Jump")]
     public float jumpForce = 10f;
     public float fallMultiplier = 2.5f;
     public float lowJumpMultiplier = 2f;
@@ -14,12 +22,21 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Walk(Vector2 dir)
     {
-        rb.velocity = (new Vector2(dir.x * speed, rb.velocity.y));
+        rb.velocity = new Vector2(dir.x * speed, rb.velocity.y);
 
+        anim.SetFloat("SpeedX", Mathf.Abs(dir.x));
+        RotatePlayer(dir.x);
+    }
+
+    private void RotatePlayer(float x)
+    {
+        if (x < 0) transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+        else if (x > 0) transform.rotation = Quaternion.Euler(0f, 0f, 0f);
     }
 
 
@@ -27,7 +44,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
 
-        if(Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump"))
         {
             rb.velocity = Vector2.up * jumpForce;
         }
@@ -43,40 +60,11 @@ public class PlayerController : MonoBehaviour
         float xInput = Input.GetAxis("Horizontal");
         float yInput = Input.GetAxis("Vertical");
 
-        /*
-        bool jumpInput = Input.GetButtonDown("Jump");
-        bool jumpInputReleased = Input.GetButtonUp("Jump");
-
-        rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
-
-        //need to add on ground conditions
-        if(jumpInput)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        }
-
-        if(jumpInputReleased && rb.velocity.y > 0)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y-gravityChange - Time.deltaTime);
-        }
-
-        if(xInput != 0)
-        {
-            transform.localScale = new Vector3(Mathf.Sign(xInput), 1, 1);
-        }
-        */
-
         Vector2 dir = new Vector2(xInput, yInput);
 
         Walk(dir);
 
-        /*
-        if(Input.GetKeyDown(KeyCode.W))
-        {
-           rb.velocity = new Vector2(rb.velocity.x, 0);
-           rb.velocity += Vector2.up * jumpForce;
-        }
-        */
+        
         
     }
 }
