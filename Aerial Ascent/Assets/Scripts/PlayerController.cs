e074dc1ca6993a409f58fdd7326057f0aed7f422
Fifi Art <fifiinart@gmail.com>
2023-07-18 18:26:44 -0400

    Player controller v2 (hopefully it works)

--- Aerial Ascent/Assets/Scripts/PlayerController.cs
+++ Aerial Ascent/Assets/Scripts/PlayerController.cs
@@ -6,142 +6,122 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Components
     private Rigidbody2D rb;
-    private Animator anim;
-    private Grappling grappleController;
-    public Transform groundCheckCollider;
 
-    [Header("Movement")]
-    public float speed = 10f;
-    public float defaultGravity = 3f;
-    [Range(0, 1)]
-    public float horizontalDamping = .5f;
-    [Range(0, 1)]
-    public float stoppedDamping = .5f;
-    [Range(0, 1)]
-    public float turningDamping = .5f;
+    [Header("Ground Collision")]
+    public LayerMask groundMask;
+    public Transform groundCheckCollider;
+    public float groundCheckRadius;
 
     [Header("Jump")]
-    public float jumpForce = 10f;
+    public float jumpForce = 5;
+
+    private float bufferJumpTimer = 0;
+    public float bufferJumpTime = 0.2f;
+
+    private float coyoteJumpTimer = 0;
+    public float coyoteJumpTime = 0.25f;
+
+    public bool onGround;
+
     [Range(0, 1)]
-    public float cutJumpHeight = .5f;
-    public bool onGround = false;
+    public float cutJumpHeight = 0.5f;
 
-    [Header("Jump Leniency")]
-    public float jumpBufferTime = .25f;
-    private float jumpBufferTimer = 0f;
-    public float coyoteTime = .25f;
-    private float coyoteTimer = 0f;
+    [Header("Movement")]
+    public float speed = 1;
 
-    [Header("Ground Check")]
-    public float groundCheckRadius = 0.2f;
-    public LayerMask groundMask;
+    // How much momentum should be preserved within 10 seconds
+    [Range(0, 1)]
+    public float horizontalDamping = 0.5f;
+    [Range(0, 1)]
+    public float stoppedDamping = 0.5f;
+    [Range(0, 1)]
+    public float turningDamping = 0.5f;
 
-    // Start is called before the first frame update
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
-        grappleController = GetComponent<Grappling>();
     }
 
-    private float CalculateXVel(float inputX)
-    {
-        float momentum = rb.velocity.x;
-        momentum += inputX;
-        momentum *= Mathf.Pow(1f - GetCorrectDamping(inputX, momentum), Time.deltaTime * 10f);
-        return momentum;
-    }
-
-    private float GetCorrectDamping(float inputX, float momentum)
-    {
-        if (Mathf.Abs(inputX) < .01) return stoppedDamping;
-        else if (Mathf.Sign(inputX) != Mathf.Sign(momentum)) return turningDamping;
-        return horizontalDamping;
-    }
-
-    private void RotatePlayer(float x)
-    {
-        if (x < 0) transform.rotation = Quaternion.Euler(0f, 180f, 0f);
-        else if (x > 0) transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-    }
-
-
-    // Update is called once per frame
     void Update()
     {
-        coyoteTimer -= Time.deltaTime;
-        jumpBufferTimer -= Time.deltaTime;
+        onGround = CheckIfGrounded();
 
-        if (onGround) coyoteTimer = coyoteTime;
-        if (Input.GetButtonDown("Jump")) jumpBufferTimer = jumpBufferTime;
+        UpdateCoyoteJumpTimer();
 
-        float xInput = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(CalculateXVel(xInput), CalculateYVel());
+        UpdateBufferJumpTimer();
 
-        anim.SetFloat("SpeedX", Mathf.Abs(xInput));
-        anim.SetFloat("SpeedY", rb.velocity.y);
-        anim.SetBool("InAir", !onGround);
-        RotatePlayer(xInput);
+        HandleCutJumps();
+
+        Jump();
+
+        float velocity = CalculateDampedVelocity();
+
+        rb.velocity = new Vector2(velocity, rb.velocity.y);
     }
 
-    private float CalculateYVel()
+    private float CalculateDampedVelocity()
     {
-        float velocity = rb.velocity.y;
-        if ((jumpBufferTimer > 0) && (coyoteTimer > 0))
-        {
-            jumpBufferTimer = 0;
-            coyoteTimer = 0;
-            velocity = jumpForce;
-        }
+        float velocity = rb.velocity.x;
+        velocity += Input.GetAxisRaw("Horizontal");
 
+        if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) < 0.01f)
+            velocity *= Mathf.Pow(1f - stoppedDamping, Time.deltaTime * 10f);
+        else if (Mathf.Sign(Input.GetAxisRaw("Horizontal")) != Mathf.Sign(velocity))
+            velocity *= Mathf.Pow(1f - turningDamping, Time.deltaTime * 10f);
+        else
+            velocity *= Mathf.Pow(1f - horizontalDamping, Time.deltaTime * 10f);
+        return velocity;
+    }
+
+    private void Jump()
+    {
+        if ((bufferJumpTimer > 0) && (coyoteJumpTimer > 0))
+        {
+            bufferJumpTimer = 0;
+            coyoteJumpTimer = 0;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    private void HandleCutJumps()
+    {
         if (Input.GetButtonUp("Jump"))
         {
             if (rb.velocity.y > 0)
             {
-               velocity *= cutJumpHeight;
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * cutJumpHeight);
             }
         }
-        return velocity;
     }
 
-    private void FixedUpdate()
+    private void UpdateBufferJumpTimer()
     {
-        UpdateGrounded();
-        //rb.gravityScale = defaultGravity;
-        //if (rb.velocity.y < 0)
-        //{
-        //    rb.gravityScale *= fallGravityMultiplier;
-        //}
-        //else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
-        //{
-        //    rb.gravityScale *= variableJumpGravityMultiplier;
-        //}
+        bufferJumpTimer -= Time.deltaTime;
+        if (Input.GetButtonDown("Jump"))
+        {
+            bufferJumpTimer = bufferJumpTime;
+        }
     }
 
-    private void UpdateGrounded()
+    private void UpdateCoyoteJumpTimer()
     {
-        onGround = false;
+        coyoteJumpTimer -= Time.deltaTime;
+        if (onGround)
+        {
+            coyoteJumpTimer = coyoteJumpTime;
+        }
+    }
+
+    private bool CheckIfGrounded()
+    {
+        //Vector2 v2GroundedBoxCheckPosition = (Vector2)transform.position + new Vector2(0, -0.01f);
+        //Vector2 v2GroundedBoxCheckScale = (Vector2)transform.localScale + new Vector2(-0.02f, 0);
+        //bool bGrounded = Physics2D.OverlapBox(v2GroundedBoxCheckPosition, v2GroundedBoxCheckScale, 0, groundMask);
+        //return bGrounded;
+
         Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheckCollider.position, groundCheckRadius, groundMask);
-        if (colliders.Length > 0)
-        {
-            onGround = true;
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.CompareTag("Tile"))
-        {
-            onGround = true;
-        }
-    }
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Tile"))
-        {
-            onGround = false;
-        }
+        return colliders.Length > 0;
     }
 }
